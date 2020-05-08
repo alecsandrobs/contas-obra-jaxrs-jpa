b80801afada028b5283ad55b593e049e144a5eaf
Alecsandro Bez Stolk <alecsandro.stolk@gmail.com>
2020-05-08 18:06:05 -0300

    consulta de lançamentos no console

--- src/main/java/com/stolk/alecsandro/obra/util/Util.java
+++ src/main/java/com/stolk/alecsandro/obra/util/Util.java
@@ -1,5 +1,6 @@
 package com.stolk.alecsandro.obra.util;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -7,6 +8,11 @@ public class Util {
 
     private static DateTimeFormatter en = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private static DateTimeFormatter br = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final DecimalFormat numerico = new DecimalFormat("#,##0.00");
+    public static final DecimalFormat real = new DecimalFormat("R$ #,##0.00");
+
+    public Util() {
+    }
 
     public static LocalDate dataBr(String data) {
         return LocalDate.parse(data, br);
@@ -22,5 +28,41 @@ public class Util {
 
     public static String dataTxtBr(LocalDate data) {
         return br.format(data);
+    }
+
+    public static String emReal(Double valor) {
+        return real.format(valor);
+    }
+
+    public static String emValor(Double valor) {
+        return numerico.format(valor);
+    }
+
+    public static String completaDireita(String texto) {
+        return completaDireita(texto, 15);
+    }
+
+    public static String completaDireita(String texto, int tamanho) {
+        int resto = tamanho - texto.length();
+        String completar = null;
+        for (int i = 0; i < resto; i++) {
+            if (i != 0) {
+                completar += " ";
+            } else {
+                completar = " ";
+            }
+        }
+        return completar + texto;
+    }
+
+    public static String completaEsquerda(String texto, int tamanho) {
+        int tamanhoTexto = texto.length();
+        if (tamanhoTexto > tamanho) {
+            return texto.substring(0, tamanho) + " ";
+        }
+        while (texto.length() <= tamanho) {
+            texto += " ";
+        }
+        return texto;
     }
 }
